c1030a2dbd4ee90fbe3a9780bbc029cfc7312950
willvm-09 <williammutugi7@gmail.com>
2024-10-01 10:28:50 +0300

    Create product model in products app

---
+++ E_commerce/products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Product (models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.CharField(max_length=200)
+    stock_quantity = models.PositiveIntegerField()
+    image = models.URLField(max_length=500)
+    Created_Date = models.DateField(auto_now_add=True)
+
