f3cae996568b22ba22d9c8364d961447ffe45a6c
willvm-09 <williammutugi7@gmail.com>
2024-10-11 01:06:32 +0300

    Change to viewsets and define url routing

--- E_commerce/users/views.py
+++ E_commerce/users/views.py
@@ -1,17 +1,10 @@
 
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
-class UserListView(generics.ListAPIView):
+class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
-class UserDetailView(generics.RetrieveAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
-
-class UserCreateView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
 
