7de4599bdb1b52841c2e08d3ac1fbce45cdc7499
willvm-09 <williammutugi7@gmail.com>
2024-10-10 21:49:27 +0300

    Create validation for stock and order quantity

--- E_commerce/products/serializers.py
+++ E_commerce/products/serializers.py
@@ -9,10 +9,29 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'name', 'description', 'price', 'category', 'stock_quantity', 'image_url', 'created_date']
     
-
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError('Price must be greater than zero.')
+        return value
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'product', 'quantity', 'order_date']
 
+    def validate_quantity(self, data):
+        product = data['product']
+        order_quantity = data['quantity']
+
+        if product.stock_quanity < order_quantity:
+             raise serializers.ValidationError(f'Not enough stock for {product.name}. Only {product.stock_quantity} left in stock')
+        return data
+    
+    def create(self, validated_data):
+        product = validated_data['product']
+        order_quantity = validated_data['quantity']
+        product.stock_quantity -= order_quantity
+        product.save()
+        order = Order.objects.create(**validated_data)
+        return order
+    
