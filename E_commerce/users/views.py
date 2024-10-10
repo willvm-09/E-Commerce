c0d52d66c5ce70664474a7069760ba52a8835945
willvm-09 <williammutugi7@gmail.com>
2024-10-11 01:13:46 +0300

    Add authentication to users views

--- E_commerce/users/views.py
+++ E_commerce/users/views.py
@@ -2,9 +2,16 @@
 from rest_framework import viewsets
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
-
+    def get(self, request):
+        return Response({"message": "You are authenticated and can see this message!"})
