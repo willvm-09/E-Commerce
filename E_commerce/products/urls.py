3fbd17de483b214d78324c79032cb5c9a6331ed1
willvm-09 <williammutugi7@gmail.com>
2024-10-10 23:38:59 +0300

    Create routing for Category viewset

--- E_commerce/products/urls.py
+++ E_commerce/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ProductViewSet, OrderViewSet
+from .views import ProductViewSet, OrderViewSet, CategoryViewSet
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'orders', OrderViewSet)
+router.register(r'category', CategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
