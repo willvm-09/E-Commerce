6dacc52c31594d668948f4db9aa51bf51d50d854
willvm-09 <williammutugi7@gmail.com>
2024-10-10 21:04:50 +0300

    Configure api views

--- E_commerce/products/views.py
+++ E_commerce/products/views.py
@@ -1,19 +1,30 @@
+from rest_framework.views import APIView
 from rest_framework import viewsets
 from .models import Product, Order
 from .serializers import ProductSerializer, OrderSerializer
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
 
 class ProductViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, IsAdminUser]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def get(self, request):
+        return Response({"message": "You are authenticated and can see this message!"})
+
 
 class OrderViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    def get(self, request):
+        return Response({"message": "You are authenticated and can see this message!"})
+
+
+
 
