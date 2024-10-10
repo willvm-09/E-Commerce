9be513d36d880d2ef3adc02486d36fb7caf5c527
willvm-09 <williammutugi7@gmail.com>
2024-10-10 23:26:47 +0300

    Register Category model

--- E_commerce/products/admin.py
+++ E_commerce/products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Order
+from .models import Product, Order, Category
 
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Category)
