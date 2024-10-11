e61f4e6f4a56ddbf6687182dfe99f5b91393ec56
willvm-09 <williammutugi7@gmail.com>
2024-10-11 20:38:26 +0300

    Apply search functionality

--- E_commerce/products/views.py
+++ E_commerce/products/views.py
@@ -5,12 +5,18 @@ from .serializers import ProductSerializer, OrderSerializer, CategorySerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
+from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ProductViewSet(viewsets.ModelViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('name', 'catergory', 'stock_quantity')
+    search_fields = ['name', 'category']
+
 
     def get(self, request):
         return Response({"message": "You are authenticated and can see this message!"})
@@ -30,6 +36,9 @@ class CategoryViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('name')
+    search_fields = ['name']
 
     def get(self, request):
         return Response({"message": "You are authenticated and can see this message!"})
